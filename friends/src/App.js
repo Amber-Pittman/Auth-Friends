0e58c44888b5041f29d130aca756af6534a90c87
Amber-Pittman <amber@amberpittman.com>
2019-11-07 08:39:06 -0500

    Working on App

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import api from "../utils/api"
 
-function App() {
-  return (
+function App(props) {
+  const signedIn = getToken();
+  // const [error, setError] = useState()
+	// const [data, setData] = useState({
+	// 	email: "",
+	// 	password: "",
+	// })
+
+	// const handleChange = (event) => {
+	// 	setData({
+	// 		...data,
+	// 		[event.target.name]: event.target.value,
+	// 	})
+	// }
+
+	// const handleSubmit = (event) => {
+	// 	event.preventDefault()
+
+	// 	// We are using are axios instance with predefined values,
+	// 	// rather than just plain old axios
+	// 	api()
+	// 		.post("/signin", data)
+	// 		.then(result => {
+	// 			// Store our new token in local storage so it persists
+	// 			localStorage.setItem("token", result.data.token)
+	// 			// Redirect the user to their account page after logging in
+	// 			props.history.push("/account")
+	// 		})
+	// 		.catch(err => {
+	// 			setError(err.response.data.message)
+	// 		})
+	}
+	
+	return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <nav>
+      <div>
+        <h2 className='logo'>Lambda Friends</h2>
+      </div>
+      <div>
+        <Link to='/' className='link'>Home</Link>
+        {!signedIn && <Link to='/signin' className='link'>Sign In</Link>}
+        {signedIn && <Link to='/account' className='link'>Account</Link>}
+      </div>
+    </nav>
+
+    <Route exact path='/signin' component={Signin} />
+    <ProtectedRoute exact path='/account' component={Account} />
+  </div>
+);
 }
 
-export default App;
+export default App

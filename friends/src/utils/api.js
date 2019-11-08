4259e63f8e7f0b89295e17ce7866d6362fb04ca3
Amber-Pittman <amber@amberpittman.com>
2019-11-07 21:03:37 -0500

    Built api for authorization header

---
+++ friends/src/utils/api.js
@@ -0,0 +1,20 @@
+// #27 Create new file for authorization header
+
+// #28 Import axios
+import axios from "axios";
+
+// #29 export a default function of the file
+export default function() {
+	return axios.create({
+		// #30 predefine the main url so you don't have to keep using the 
+		//   entire url over and over		
+		baseURL: "http://localhost:8080", // we'll never have to redefine it
+		
+		// #31 Predefine some headers to this axios instance
+		headers: {
+			// custom header
+			// #61 Just call getToken for Authorization
+			Authorization: getToken(),
+		},
+	})
+}

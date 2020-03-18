1b8869ff6c53881d1da708bc06ab0e2fdc83c982
Amber-Pittman <amber@amberpittman.com>
2020-03-17 20:04:38 -0400

    Destructured props & added render to Route in return

--- friends/src/components/PrivateRoute.js
+++ friends/src/components/PrivateRoute.js
@@ -1,8 +1,20 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, Router } from "react-router-dom";
 
 export default function PrivateRoute(props) {
+    const { // Destructure from props here
+        component: Component,
+        ...rest
+    } = props
 
-    return ()
+    return (
+        <Route {...rest} render={(renderProps) => {
+            if (localStorage.getItem("token")) {
+                return Component {...renderProps} />
+            } else {
+                return <Redirect to="/login" />
+            }
+        }}
+    )
 
 }
